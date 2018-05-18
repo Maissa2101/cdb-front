3f01230d1483e27a6ba2217ce517ec67f9a98367
jirr <jirr@excilys.com>
2018-05-18 16:20:33 +0200

    [FEAT] (Add) Component and form for Create feature

--- src/app/company/create-company/create-company.component.ts
+++ src/app/company/create-company/create-company.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CompanyService } from '../company.service';
+import { Company } from '../company.model';
 
 @Component({
   selector: 'app-create-company',
   templateUrl: './create-company.component.html',
   styleUrls: ['./create-company.component.css']
 })
-export class CreateCompanyComponent implements OnInit {
+export class CreateCompanyComponent {
+  companyGroup: FormGroup;
 
-  constructor() { }
+  constructor(private companyService: CompanyService, private formBuilder: FormBuilder) {
+    this.createForm();
+  }
 
-  ngOnInit() {
+  createForm() {
+    this.companyGroup = this.formBuilder.group({
+    name: ["", Validators.required],
+    logo: ""
+    })
+  }
+
+  onSubmit() {
+    let value = this.companyGroup.value;
+    let company: Company = {
+      id: 0,
+      name: value.name,
+      logo: value.logo
+    };
+    this.companyService.addCompany(company);
   }
 
 }
