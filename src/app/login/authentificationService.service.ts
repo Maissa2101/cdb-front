4f93f65fe10bdcc8b57b98f5c5da5f27fe991d2e
pnexer <pnexer@excilys.com>
2018-05-25 17:56:43 +0200

    [Fix] message error login

--- src/app/login/authentificationService.service.ts
+++ src/app/login/authentificationService.service.ts
@@ -1,13 +1,12 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthenticationService {
 
   private baseUrl = 'http://10.0.1.94:8080/cdb-webservice/users/';
 
-  constructor(private http: HttpClient, private router: Router) {
+  constructor(private http: HttpClient) {
   }
 
   public getToken(): string {
@@ -19,40 +18,21 @@ export class AuthenticationService {
     return localStorage.getItem('isAdmin');
   }
 
-  public noErrorlogin(token: any) {
-
-    localStorage.setItem('token', token);
-    localStorage.setItem('isAdmin', 'true');
-    console.warn('user connected');
-    this.router.navigate(['companies']);
-  }
-
-  public noErrorlogout() {
-
-    localStorage.removeItem('token');
-    console.info('user disconnected');
-    this.router.navigate(['login']);
-  }
-
-  public login(login: string, password: string) {
+  login(login: string, password: string){
 
     let headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
     let body = "login=" + login + "&password=" + password;
 
-    return this.http.post(`${this.baseUrl}login`, body,
-      {headers: headers, responseType: 'text'})
-      .subscribe(
-        token => this.noErrorlogin(token),
-        error => {
-          console.error('Authentification problem', error), localStorage.removeItem('token')
-        });
+    return  this.http.post(`${this.baseUrl}login`, body,
+      {headers: headers, responseType: 'text'});
+
   }
 
   public logout() {
     return this.http.post(`${this.baseUrl}logout`,
       {},
-      {responseType: 'text'}).subscribe(() => this.noErrorlogout(),
-      error1 => console.error('error in logout'))
+      {responseType: 'text'}).subscribe(
+      error => console.error('error in logout'))
   }
 
 }
--- src/app/login/login-page/login.component.ts
+++ src/app/login/login-page/login.component.ts
@@ -5,6 +5,8 @@ import {User} from "../user.model";
 import {UserService} from "../user.service";
 import {AlertService} from "../alert.service";
 import {AuthenticationService} from "../authentificationService.service";
+import { MatSnackBar } from '@angular/material';
+
 
 @Component({
   selector: 'app-login',
@@ -13,44 +15,79 @@ import {AuthenticationService} from "../authentificationService.service";
 })
 export class LoginComponent implements OnInit {
 
-    user: User;
-    userForm:  FormGroup;
+  user: User;
+  userForm:  FormGroup;
 
-    constructor(
-        private router: Router,
-        private activatedRoutes: ActivatedRoute,
-        private fb: FormBuilder,
-        private userService: UserService,
-        private alertService: AlertService,
-        private  authentificationService: AuthenticationService
-    ) { }
+  constructor(
+    private router: Router,
+    private activatedRoutes: ActivatedRoute,
+    private fb: FormBuilder,
+    private userService: UserService,
+    private alertService: AlertService,
+    private  authentificationService: AuthenticationService,
+    public snackBar: MatSnackBar
+  ) { }
 
-    ngOnInit() {
-        this.user = new User();
-        this.createForm();
-    }
+  public noErrorLogin(token: any) {
 
-    createForm(){
-        this.userForm = this.fb.group({
-            user: ["",Validators.required],
-            password: ["",Validators.required]
-        });
-    }
+    localStorage.setItem('token', token);
+    localStorage.setItem('isAdmin', 'true');
+    console.info('user connected');
+    this.router.navigate(['companies']);
 
-    onSubmit(){
-        this.user.login = this.userForm.controls.user.value;
-        this.user.password = this.userForm.controls.password.value;
-        this.authentificationService.login(this.user.login,this.user.password);
-    }
+    this.snackBar.open("Login success", "Close", {
+      panelClass: 'snackbar-ok',
+      duration: 2500,
+    });
+  }
 
-    onClickSignOut(){
-        this.authentificationService.logout();
+  public errorLogin(){
 
-    }
+    console.error('Authentification problem');
+    localStorage.removeItem('token');
 
-    onClickSignUp(){
-        console.log('sign up page');
-        this.router.navigate(['loginAdd']);
-    }
+    this.snackBar.open("Unrecognized user.", "Close", {
+      panelClass: 'snackbar-error',
+      duration: 2500,
+    });
+}
+
+  public noErrorlogout() {
+
+    localStorage.removeItem('token');
+    console.info('user disconnected');
+    this.router.navigate(['login']);
+  }
+
+  ngOnInit() {
+    this.user = new User();
+    this.createForm();
+  }
+
+  createForm(){
+    this.userForm = this.fb.group({
+      user: ["",Validators.required],
+      password: ["",Validators.required]
+    });
+  }
+
+  onSubmit(){
+    this.user.login = this.userForm.controls.user.value;
+    this.user.password = this.userForm.controls.password.value;
+    this.authentificationService.login(this.user.login,this.user.password).subscribe(
+      token => this.noErrorLogin(token),
+      () => this.errorLogin()
+    );
+
+  }
+
+  onClickSignOut(){
+    this.authentificationService.logout();
+  }
+
+  onClickSignUp(){
+    console.log('sign up page');
+    this.router.navigate(['loginAdd']);
+  }
 
 }
